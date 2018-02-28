36c533a4059667aed9b5a30fad2dd16d877e43c2
norvylle <nsuy@up.edu.ph>
2018-02-28 23:21:16 +0800

    updated App.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './../node_modules/semantic-ui/dist/semantic.min.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
